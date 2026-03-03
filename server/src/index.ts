c4aa6a5d298166823079d1676acd7c8df1328184
Yuvansh <chauan.boby0211@gmail.com>
2026-03-04 01:49:16 +0530

    schema design

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
+import Routes from "./routes/index.js";
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
@@ -13,5 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req: Request, res: Response) => {
   return res.send("It's working 🙌");
 });
+
+//Routes
+app.use("/api", Routes);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
