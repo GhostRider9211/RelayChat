8891882afcffc75bb9646e80cfa748c6d1ced647
Yuvansh <chauan.boby0211@gmail.com>
2026-03-08 00:02:53 +0530

    Changes made in socket.ts

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,13 +1,28 @@
 import {Server} from "socket.io";
+import {Socket} from "socket.io";
 
+interface CustomSocket extends Socket{
+room?:string;
+}
 export function setupSocket(io:Server){
-    io.on("connection",(socket)=>{
+    io.use((socket:CustomSocket,next)=>{
+        const room = socket.handshake.auth.room;
+        
+        if(!room){
+            return next(new Error("Invalid room"));
+        }
+        socket.room = room;
+
+        next(); 
+    })
+    io.on("connection",(socket:CustomSocket)=>{
+        
         console.log("A user connected with id: ",socket.id);
 
 
         socket.on("message",(data)=>{
             console.log("The socket message is:",data);
-            socket.broadcast.emit("message",data);
+            io.to(socket.room!).emit("message",data);
         });
         socket.on("disconnect",()=>{
             console.log("A user disconnected with id: ",socket.id);
