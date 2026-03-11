c5984f0fca7ddf9e74975bf1358f5605eb33070c
Yuvansh <chauan.boby0211@gmail.com>
2026-03-12 00:22:57 +0530

    Add Docker setup and switch Redis to Upstash

--- relaychat/next.config.ts
+++ relaychat/next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   reactCompiler: true,
-  reactStrictMode:false,
+  reactStrictMode: false,
 };
 
 export default nextConfig;
